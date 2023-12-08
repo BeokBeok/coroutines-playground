4f8a04cc930c530fa056486cc09dd45cd4e3650d
강현석 <kekemusa37@gmail.com>
2023-12-09 08:48:51 +0900

    [추가] launch와 dispatcher 관련 테스트 코드

--- app/src/main/java/com/beok/playground/MainActivity.kt
+++ app/src/main/java/com/beok/playground/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.beok.playground
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,7 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.beok.playground.ui.theme.CoroutinesplaygroundTheme
+import kotlin.random.Random
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +29,42 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                }
+            }
+        }
+
+//        printLogOneLaunch()
+//        printLogMultiLaunch()
+    }
+
+    private fun printLogOneLaunch(dispatcher: CoroutineDispatcher = Dispatchers.IO) {
+        lifecycleScope.launch(dispatcher) {
+            repeat(1_000) {
+                List(1_000) {
+                    Random.nextLong()
+                }.maxOrNull()
+
+                Log.d("kurt", "Running on thread: ${Thread.currentThread().name}")
+            }
+        }
+    }
+
+    /**
+     * 하나의 launch에는 하나의 Dispatcher가 구동된다.
+     * 즉, tid(6318)가 같으면 dispatcher(DefaultDispatcher-worker-15)도 같다
+     *
+     * 08:44:34.932 6276-6318 Running on thread: DefaultDispatcher-worker-15
+     * 08:44:34.933 6276-6318 Running on thread: DefaultDispatcher-worker-15
+     */
+    private fun printLogMultiLaunch(dispatcher: CoroutineDispatcher = Dispatchers.IO) {
+        lifecycleScope.launch {
+            repeat(1_000) {
+                launch(dispatcher) {
+                    List(1_000) {
+                        Random.nextLong()
+                    }.maxOrNull()
+
+                    Log.d("kurt", "Running on thread: ${Thread.currentThread().name}")
                 }
             }
         }
